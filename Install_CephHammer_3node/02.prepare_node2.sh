82cfa4a15ab6c1b9052ec87eb4ca3436c6c86bcc
Quang Long <longsube@gmail.com>
2015-11-10 12:07:30 +0700

    Sua lai viec dat IP

--- Install_CephHammer_3node/02.prepare_node2.sh
+++ Install_CephHammer_3node/02.prepare_node2.sh
@@ -3,10 +3,10 @@ source config.cfg
 
 echo "########## Dat IP cho NIC ##########"
 ETH0=/etc/sysconfig/network-scripts/ifcfg-eth0
-test -f ETH0.orig || cp ETH0 ETH0.orig
-rm ETH0
-touch ETH0
-cat << EOF >  ETH0
+test -f $ETH0.orig || cp $ETH0 ETH0.orig
+rm $ETH0
+touch $ETH0
+cat << EOF >  $ETH0
 DEVICE=eth0
 TYPE=Ethernet
 ONBOOT=yes
@@ -19,10 +19,10 @@ DNS1=8.8.8.8
 EOF
 
 ETH1=/etc/sysconfig/network-scripts/ifcfg-eth1
-test -f ETH1.orig || cp ETH1 ETH1.orig
-rm ETH1
-touch ETH1
-cat << EOF >  ETH1
+test -f $ETH1.orig || cp $ETH1 $ETH1.orig
+rm $ETH1
+touch $ETH1
+cat << EOF >  $ETH1
 DEVICE=eth1
 TYPE=Ethernet
 ONBOOT=yes
@@ -32,12 +32,12 @@ IPADDR=$CEPH2_LOCAL
 NETMASK=255.255.255.0
 EOF
 
-ETH2=/etc/sysconfig/network-scripts/ifcfg-eth2
-test -f ETH2.orig || cp ETH2 ETH2.orig
-rm ETH2
-touch ETH2
-cat << EOF >  ETH2
-DEVICE=eth2
+ETH2=/etc/sysconfig/network-scripts/ifcfg-$ETH2
+test -f $ETH2.orig || cp $ETH2 $ETH2.orig
+rm $ETH2
+touch $ETH2
+cat << EOF >  $ETH2
+DEVICE=$ETH2
 TYPE=Ethernet
 ONBOOT=yes
 NM_CONTROLLED=yes
--- Kilo_Ceph_integrated.md
+++ Kilo_Ceph_integrated.md
@@ -16,10 +16,11 @@ Chuẩn bị môi trường:
 - Các máy ảo có 3 card mạng tương ứng với các dải mạng Local, External và Replicate
 
 ####C.1. Truy cập bằng tài khoản root vào máy các máy chủ và tải các gói, script chuẩn bị cho quá trình cài đặt
-	yum update
+	yum update -y
 	yum install git -y
-	git clone
-	mv /root/https://github.com/longsube/CEPH_3Net_Openstack/Install_CephHammer_3node
+	git clone https://github.com/longsube/CEPH_3Net_Openstack
+	mv CEPH_3Net_Openstack/Install_CephHammer_3node/ Install_CephHammer_3node/
+	rm -rf CEPH_3Net_Openstack/
 	cd Install_CephHammer_3node/
 	chmod +x *.sh
 
